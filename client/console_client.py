c65b958bf024a2413e973bd6fb995fc16b96a193
Veli Viisanen <veli.viisanen@gmail.com>
2021-05-10 19:38:16 +0300

    console client update

--- client/console_client.py
+++ client/console_client.py
@@ -14,16 +14,21 @@ def clear_screen():
         print('Unknown operating system. Unable to clear screen.')
 
 def poll_scanner(device_id='c44f330a102d', port=65432):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((socket.gethostbyname(device_id), port))
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((socket.gethostbyname(device_id), port))
 
-    data = s.recv(1024)
+        data = s.recv(1024)
 
-    if not data:
-        pass
-    else:
-        return json.loads(data)
-        s.close()
+        if not data:
+            pass
+        else:
+            s.close()
+            return json.loads(data)
+    
+    except Exception as e:
+        print(e)
+        return None
 
 def main_menu():
     clear_screen()
@@ -56,6 +61,12 @@ def identify_materials():
         if mode == "":
             print("Polling scanner...")
             data = poll_scanner()
+
+            if not data:
+                print('error while polling scanner...')
+                input('press any kay to return to main menu...')
+                break
+
             print("Retrieved spectrum:", data["spectrum"])
 
             df = pd.DataFrame(data["spectrum"])
@@ -68,9 +79,29 @@ def identify_materials():
 
 def retrieve_spectrum_data():
     clear_screen()
-    material_type = "cotton" # change material type accordingly
+    materials_in_db = pd.unique(spectrum_db['type'])
     
     print("# Retrieve & save spectrum data from scanner\n")
+    print("choose material type...\n")
+
+    while True:
+        for idx, material in enumerate(materials_in_db):
+            print(f'{ str(idx) })'.rjust(10), material)
+
+        print('\n' + 'n)'.rjust(10) + ' add new material')
+
+        material = input('\ninput: ')
+
+        if material.isnumeric() and int(material) in range(len(materials_in_db)):
+            material = materials_in_db[int(material)]
+            break
+        elif material == 'n':
+            material = input('Enter new material type: ').lower()
+            break
+        else:
+            continue
+
+    print(f'\nRetrieving { material } spectrum data...\n')
 
     print("enter)".rjust(10) + " Poll scanner")
     print("m)".rjust(10) + " Return to main menu\n")
@@ -81,15 +112,21 @@ def retrieve_spectrum_data():
         if mode == "":
             print("Polling scanner...")
             data = poll_scanner()
+
+            if not data:
+                print('error while polling scanner...')
+                input('press any kay to return to main menu...')
+                break
+
             print("Retrieved spectrum:", data["spectrum"])
 
             new_db_entry = data["spectrum"]
-            new_db_entry.append(material_type)
+            new_db_entry.append(material)
 
             with open("spectrum_db.csv", "a") as f:
                 f.write("\n" + str(new_db_entry).strip("[]").replace("'", ""))
 
-            print("saved to db as " + material_type)
+            print("saved to db as " + material)
 
         elif mode == "m":
             break
